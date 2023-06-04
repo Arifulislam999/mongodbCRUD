6dffca98e03ce05a78914cebc998d0c3028a2d2e
Ariful Islam <arifulislamjoy597.com>
2023-06-04 14:13:25 +0600

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main>
+      <h2>This is Next js Project.</h2>
+    </main>
+  );
+}
